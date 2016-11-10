84057027658d90e7f8f9ea372192a21f64912c55
JD Rosensweig <jd@xyleolabs.com>
2016-11-10 14:12:35 -0800

    Upgraded unit test to uber unit test.
       Used Junit Rule to handle expected exceptions.  Allows for more granularity.

--- verteiler/src/main/java/io/datanerds/verteiler/KafkaConfigValidator.java
+++ verteiler/src/main/java/io/datanerds/verteiler/KafkaConfigValidator.java
@@ -5,6 +5,9 @@ import java.util.Properties;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG;
 
 final class KafkaConfigValidator {
+    static final String NULL_PROPS_MESSAGE = "Kafka properties must not be null";
+    static final String AUTO_COMMIT_ENABLED_MSG =
+            String.format("%s must not be enabled for the BlockingQueueConsumer", ENABLE_AUTO_COMMIT_CONFIG);
 
     private KafkaConfigValidator() {
     }
@@ -16,15 +19,14 @@ final class KafkaConfigValidator {
 
     private static void verifyPropsNotNull(Properties props) {
         if (props == null) {
-            throw new IllegalArgumentException("Kafka properties must not be null");
+            throw new IllegalArgumentException(NULL_PROPS_MESSAGE);
         }
     }
 
     private static void verifyAutoCommitDisabled(Properties props) {
         Boolean isAutoCommitEnabled = Boolean.parseBoolean(props.getProperty(ENABLE_AUTO_COMMIT_CONFIG));
         if (isAutoCommitEnabled) {
-            throw new IllegalArgumentException(
-                    String.format("%s must not be enabled for the BlockingQueueConsumer", ENABLE_AUTO_COMMIT_CONFIG));
+            throw new IllegalArgumentException(AUTO_COMMIT_ENABLED_MSG);
         }
     }
 }
