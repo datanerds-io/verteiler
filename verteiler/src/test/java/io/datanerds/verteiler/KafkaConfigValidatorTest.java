aef5dbd391b0b85e82404652cff0d1a40d229821
JD Rosensweig <jd@xyleolabs.com>
2016-11-10 14:12:35 -0800

    Upgraded unit test to uber unit test.
       Used Junit Rule to handle expected exceptions.  Allows for more granularity.

--- verteiler/src/test/java/io/datanerds/verteiler/KafkaConfigValidatorTest.java
+++ verteiler/src/test/java/io/datanerds/verteiler/KafkaConfigValidatorTest.java
@@ -1,17 +1,23 @@
 package io.datanerds.verteiler;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Properties;
 
+import static io.datanerds.verteiler.KafkaConfigValidator.*;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG;
 
 @RunWith(MockitoJUnitRunner.class)
-public class BlockingQueueConfigValidatorTest {
+public class KafkaConfigValidatorTest {
     private Properties testProps;
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -21,17 +27,23 @@ public class BlockingQueueConfigValidatorTest {
 
     @Test
     public void testValidate() throws Exception {
-        KafkaConfigValidator.validate(testProps);
+        validate(testProps);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testValidateAutoCommitTrue() throws Exception {
+        expected.expect(IllegalArgumentException.class);
+        expected.expectMessage(AUTO_COMMIT_ENABLED_MSG);
+
         testProps.setProperty(ENABLE_AUTO_COMMIT_CONFIG, "true");
-        KafkaConfigValidator.validate(testProps);
+        validate(testProps);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testValidateNullProperties() throws Exception {
-        KafkaConfigValidator.validate(null);
+        expected.expect(IllegalArgumentException.class);
+        expected.expectMessage(NULL_PROPS_MESSAGE);
+
+        validate(null);
     }
 }
